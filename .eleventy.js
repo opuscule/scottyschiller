614fec5b01254457527f65ca0a6c00954346cf95
Robert C. Bowman III <bowman@opuscule.com>
2022-11-09 15:59:15 -0500

    Articles UI

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
 	
 	eleventyConfig.addPassthroughCopy('./src/assets');
 	eleventyConfig.addPassthroughCopy('./src/admin');
+	eleventyConfig.addPassthroughCopy('./src/images');
 	eleventyConfig.addFilter("postDate", (dateObj) => {
 		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_SHORT);
 	})
